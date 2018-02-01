1cabb4785cc8585a5c941f5ae3e84ca01bdb210e
KarlChuang <karl0930@gmail.com>
2018-02-01 12:16:50 +0800

    Init a project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const inProduction = (process.env.NODE_ENV == 'inProduction');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist/bundle'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rule: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use:['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [],
+}
+
+if (inProduction) {
+  module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  module.exports.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+    },
+  }));
+}
