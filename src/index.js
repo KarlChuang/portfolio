d51e863b80c03661f5c890b5f0e1352a4c41cae7
KarlChuang <karl0930@gmail.com>
2018-05-16 01:10:55 +0800

    remove redux-logger

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import logger from 'redux-logger';
 import Root from './Components/Root';
 import rootReducer from './Reducers/root';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
